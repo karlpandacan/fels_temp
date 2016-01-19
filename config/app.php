d2bf8a5692cfc11c70bb193c9e2bc1f45e4fb8b1
Karl Pandacan <karlpandacan@yahoo.com>
2016-01-19 11:33:10 +0800

    Change Vendor Illuminate/Html to Collective/Html

--- config/app.php
+++ config/app.php
@@ -159,8 +159,7 @@ return [
         /*
          * Added Providers
          */
-        Illuminate\Html\HtmlServiceProvider::class,
-
+        Collective\Html\HtmlServiceProvider::class,
     ],
 
     /*
@@ -206,9 +205,8 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
-        'Form'      => Illuminate\Html\FormFacade::class,
-        'HTML'      => Illuminate\Html\HtmlFacade::class,
-
+        'Form'      => Collective\Html\FormFacade::class,
+        'Html'      => Collective\Html\HtmlFacade::class
     ],
 
 ];
